1c04b0f5987e70df8db059b1c4a8c32ba1520216
kunal00000 <kunalverma2468@gmail.com>
2022-10-12 14:43:50 +0530

    ui changes

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let allElement = document.querySelectorAll('.letter');
-// let allBtns = document.querySelectorAll('.btn');
+let allBtns = document.querySelectorAll('.btn');
 
 // for(let bt of allBtns){
 //     bt.addEventListener('click',function(){
@@ -10,6 +10,8 @@ let wordsList = ['HURRY','LOOSE','THEME','BLOCK','BRAVE','WORDS','HAIRS','KILLS'
 
 const randWord = wordsList[Math.floor(Math.random() * wordsList.length)];
 console.log(randWord);
+
+allElement[0].focus();
 
 for(let b of allElement){
     b.addEventListener('keydown',function(ev){
@@ -22,21 +24,26 @@ for(let b of allElement){
                 this.value="";
                 this.parentElement.previousElementSibling.firstElementChild.focus();
             }
+            this.classList.remove('letterOutline');
         }
     })
-    b.addEventListener('keyup',function(ev){
-        this.classList.add('letterOutline');
+    b.addEventListener('keydown',function(ev){
         if(this.parentElement.parentElement.lastElementChild == this.parentElement & this.value!="" & ev.code=='Enter'){
             // Match the word if in array
             let found=[false,false,false,false,false];
             for(let i=0;i<5;i++){
                 for(let j=0;j<5;j++){
+                    let x = this.parentElement.parentElement.children[i].firstElementChild.value.toUpperCase().charCodeAt();
                     if(randWord[i] == this.parentElement.parentElement.children[i].firstElementChild.value.toUpperCase()){
                         this.parentElement.parentElement.children[i].firstElementChild.classList.add('correct');
+                        if(allBtns[x-65].classList.contains('present')) allBtns[x-65].classList.remove('present');
+                        allBtns[x-65].classList.add('correct');
                         found[i]=true;
                     }
                     else if((i!=j) & (randWord[j]==this.parentElement.parentElement.children[i].firstElementChild.value.toUpperCase())){
                         this.parentElement.parentElement.children[i].firstElementChild.classList.add('present');
+                        if(allBtns[x-65].classList.contains('correct')) allBtns[x-65].classList.remove('correct');
+                        allBtns[x-65].classList.add('present');
                         if(found[i]){
                             found[i]=false;
                         }
@@ -48,6 +55,8 @@ for(let b of allElement){
             for(let i=0;i<5;i++){
                 if(!found[i]){
                     this.parentElement.parentElement.children[i].firstElementChild.classList.add('wrong');
+                    let y = this.parentElement.parentElement.children[i].firstElementChild.value.toUpperCase().charCodeAt();
+                    allBtns[y-65].classList.add('wrong');
                 }
                 else{
                     countTrue++;
@@ -62,9 +71,11 @@ for(let b of allElement){
             if(this.parentElement.parentElement.lastElementChild != this.parentElement & this.value!=""){
                 this.parentElement.nextElementSibling.firstElementChild.focus();
             }
+            this.classList.add('letterOutline');
         }
         else{
             this.value="";
+            ev.preventDefault();
         }
     })
 }
